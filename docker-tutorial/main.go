09720e894eb6746e63800f428d88d52c40169502
Shun Yamada <shantianshun42@gmail.com>
2026-08-24 13:48:16 +0900

    データの永続化を学習

--- docker-tutorial/main.go
+++ docker-tutorial/main.go
@@ -7,7 +7,7 @@ import(
 func main(){
     http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
     fmt.Printf("[%s]リクエスト受信:\n",time.Now().Format(time.RFC3339))
-    fmt.Fprintf(w,"Hello,Docker This is a simple Go server.")
+    fmt.Fprintf(w, "Hello, Docker Update Success")
     })
     fmt.Println("サーバーを8080番ポートで起動します")
     if err :=http.ListenAndServe(":8080",nil); err != nil{
